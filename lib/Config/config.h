6f69cd79bcf179d27ef2e14984983fcc616c558e
NEURON <50287102+neuron909@users.noreply.github.com>
2020-11-14 20:09:07 +0530

    Added: Config of Motor, Pin, Ultrasonic

--- lib/Config/config.h
+++ lib/Config/config.h
@@ -1,9 +1,14 @@
 // Pin Configuration
-#define RELAY_MODULE_PIN 5
-#define ULTRASONIC_TRIG_PIN 10
-#define ULTRASONIC_ECHO_PIN 10
+#define RELAY_MODULE_PIN 8
+
+#define ULTRASONIC_TRIG_PIN 7
+#define ULTRASONIC_ECHO_PIN 6
 
 
 // Motor
 #define MOTOR_MAX_RUNTIME_IN_MS 100 
+
+// Ultrasonic Sensor
+#define ULTRASONIC_TRIGGER_DIST_IN_CM 20
+
 
