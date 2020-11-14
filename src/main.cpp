60a5e4325b1483b006587f612d6fa8801cf27729
NEURON <50287102+neuron909@users.noreply.github.com>
2020-11-14 19:57:32 +0530

    Added: Ultrosonic TestCode

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
-#include <Arduino.h>
+#include <NewPing.h>
+#include<config.h>
+
+NewPing sonar( ULTRASONIC_TRIG_PIN, ULTRASONIC_ECHO_PIN ); // NewPing setup of pins and maximum distance.
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(250);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  unsigned int uS = sonar.ping_cm(); // Send ping, get ping time in microseconds (uS).
+  Serial.print("Ping: ");
+  Serial.print(uS); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+  Serial.print("cm");
+  if( uS > 20 ) Serial.println();
+  else Serial.println(" aaaaaaaa");
 }
